362a8b8df87c933e667c160ac666463d5d5e08a4
men1692 <men16922@gmail.com>
2022-09-23 18:28:25 +0900

    [feat] mysql 연동

--- src/main/java/io/springbatch/springbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/JobConfiguration.java
@@ -12,55 +12,42 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
 @Configuration
-public class HelloJobConfiguration {
+@RequiredArgsConstructor
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep1())
-                .next(helloStep2())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
-    @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
+    private Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.printf("==========================");
-                        System.out.println(">> Hello Spring Batch!!");
-                        System.out.printf("==========================");
-
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
 
-    @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
+    private Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-                        System.out.printf("==========================");
-                        System.out.println(">> step2 was executed");
-                        System.out.printf("==========================");
-
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
-
-
 }
