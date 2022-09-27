6c9307e5fc8701167162cbbc79c2203ec54b775f
men1692 <men16922@gmail.com>
2022-09-27 17:15:50 +0900

    [feat] JobLauncher

--- src/main/java/io/springbatch/springbatch/JobRepositoryConfiguration.java
+++ src/main/java/io/springbatch/springbatch/JobRepositoryConfiguration.java
@@ -2,6 +2,7 @@ package io.springbatch.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -14,37 +15,38 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class StepContributionConfiguration {
+public class JobRepositoryConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+
+    private final JobExecutionListener jobRepositoryListener;
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .listener(jobRepositoryListener)
                 .build();
     }
 
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
 
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(((contribution, chunkContext) -> null))
                 .build();
     }
 
