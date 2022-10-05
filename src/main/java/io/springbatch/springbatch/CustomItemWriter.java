4fd0497ba9f9891b78ac2b6fc3b79a58bb718090
men1692 <men16922@gmail.com>
2022-10-05 14:45:20 +0900

    [feat] ItemStream

--- src/main/java/io/springbatch/springbatch/CustomItemWriter.java
+++ src/main/java/io/springbatch/springbatch/CustomItemWriter.java
@@ -1,14 +1,30 @@
 package io.springbatch.springbatch;
 
-
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamWriter;
 
 import java.util.List;
 
-public class CustomItemWriter implements ItemWriter<Customer> {
+public class CustomItemWriter implements ItemStreamWriter<String> {
 
     @Override
-    public void write(List<? extends Customer> items) throws Exception {
+    public void write(List<? extends String> items) throws Exception {
         items.forEach(item -> System.out.println(item));
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        System.out.println("open");
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        System.out.println("update");
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+        System.out.println("close");
     }
 }
--- src/main/java/io/springbatch/springbatch/ItemStreamConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ItemStreamConfiguration.java
@@ -5,18 +5,17 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class ItemReader_ItemProcessor_ItemWriterConfiguration {
+public class ItemStreamConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -32,28 +31,26 @@ public class ItemReader_ItemProcessor_ItemWriterConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(3)
+                .<String, String>chunk(5)
                 .reader(itemReader())
-                .processor(itemProcessor())
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
-    public ItemWriter<? super Customer> itemWriter() {
+    public CustomItemStreamReader itemReader() {
+        List<String> items = new ArrayList<>(10);
+
+        for(int i = 1; i <= 10; i++) {
+            items.add(String.valueOf(i));
+        }
+
+        return new CustomItemStreamReader(items);
+    }
+
+    @Bean
+    public ItemWriter itemWriter() {
         return new CustomItemWriter();
-    }
-
-    @Bean
-    public ItemProcessor<? super Customer,? extends Customer> itemProcessor() {
-        return new CustomItemProcessor();
-    }
-
-    @Bean
-    public ItemReader<Customer> itemReader() {
-        return new CustomItemReader(Arrays.asList(new Customer("user1"),
-                new Customer("user2"),
-                new Customer("user3")));
     }
 
     @Bean
