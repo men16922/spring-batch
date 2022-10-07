4da8a8afbaee69b0d54ca6e73736a89a267cc3ba
men1692 <men16922@gmail.com>
2022-10-07 09:35:56 +0900

    [feat] JdbcCursorItemReader

---
+++ src/main/java/io/springbatch/springbatch/Customer.java
@@ -0,0 +1,12 @@
+package io.springbatch.springbatch;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
