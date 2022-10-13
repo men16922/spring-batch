dc7f0c5fa95a04031fb667ed4ea4f2e7617d8d0a
men1692 <men16922@gmail.com>
2022-10-13 09:14:43 +0900

    [feat] FaultTolerant

---
+++ src/main/java/io/springbatch/FaultTolerantConfiguration.java
@@ -0,0 +1,63 @@
+package io.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class FaultTolerantConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private int chunkSize = 10;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(chunkSize)
+                .reader(new ItemReader<String>() {
+                    int i = 0;
+
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        i++;
+                        if(i == 1) {
+                            throw new IllegalArgumentException("this exception is skipped");
+                        }
+                        return i > 3 ? null : "item" + i;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+
+                        throw new IllegalStateException("this exception is retry");
+//                        return null;
+                    }
+                })
+                .writer(items -> System.out.println(items))
+                .faultTolerant()
+                .skip(IllegalArgumentException.class)
+                .skipLimit(2)
+                .retry(IllegalStateException.class)
+                .retryLimit(2)
+                .build();
+    }
+
+}
+
